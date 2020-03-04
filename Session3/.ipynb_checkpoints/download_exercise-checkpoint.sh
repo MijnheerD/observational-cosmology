1f52e9028746e1c834ac0787bd242c7b43189bc1
Mitja <mitja.desmet@epfl.ch>
2020-03-04 11:03:31 +0000

    Made session 3

---
+++ Session3/.ipynb_checkpoints/download_exercise-checkpoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script downloads the exercise jupyter notebooks.
+# Just put all the ones you want to download into the NOTEBOOKS array.
+
+ROOT_URL="https://obswww.unige.ch/~ivkovic/homepage/astro-iv"
+NOTEBOOKS=(2PCF)
+
+if [ "$ROOT_URL" == "" ]; then
+  echo "Error: ROOT_URL not set!"
+  exit
+fi
+
+source ${HOME}/jnb.conf
+if [ "$MY_NOTEBOOKS" == "" ]; then
+  MY_NOTEBOOKS=${HOME}/AstroIV/Session3
+fi
+
+cd $MY_NOTEBOOKS
+
+for NB in ${NOTEBOOKS[@]}; do
+  URL=${ROOT_URL}/E_jupyter_${NB}.ipynb
+  wget $URL
+done
+
